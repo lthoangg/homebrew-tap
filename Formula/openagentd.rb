98ca869fd53796bbcb3226f0f00034f2c2a0a91d
Hoang Le <59216583+lthoangg@users.noreply.github.com>
2026-04-30 11:34:16 +0700

    fix: clean up formula (remove premature bottle block)

--- Formula/openagentd.rb
+++ Formula/openagentd.rb
@@ -6,24 +6,11 @@ class Openagentd < Formula
   url "https://files.pythonhosted.org/packages/source/o/openagentd/openagentd-0.1.0.tar.gz"
   sha256 "PLACEHOLDER_SHA256"
   license "Apache-2.0"
-  head "https://github.com/lthoangg/openagentd.git", branch: "main"
-
-  bottle do
-    rebuild 1
-    root_url "https://ghcr.io/v2/lthoangg/tap"
-  end
 
   depends_on "python@3.14"
-  depends_on "uv" => :build
 
   def install
-    # Install into an isolated virtualenv managed by Homebrew
     virtualenv_install_with_resources
-  end
-
-  def post_install
-    # Create XDG config directory so first-run init has a home
-    (var/"openagentd").mkpath
   end
 
   test do
